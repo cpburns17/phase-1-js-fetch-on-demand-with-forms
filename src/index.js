0d62be4d261f4b33ed1730dfe9bf3681e60e88e9
Connor Burns <cpburns17@gmail.com>
2023-11-05 15:08:15 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
+
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');   
+    
+        console.log(input.value);
+
+        fetch (`http://localhost:3000/movies/${input.value}`)
+        .then ((resp) => resp.json())
+        .then ((data) => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            }
+        );
+    });
 }
 
+
+const title = document.querySelector('section#movieDetails h4');
+const Summary = document.querySelector('section#movieDetails p');
+
+
 document.addEventListener('DOMContentLoaded', init);
+
+
+// is const inputForm = document.querySelector('event.target.children[1].value') the same as 
+// const inputForm = document.querySelector('input#searchByID'); ?
